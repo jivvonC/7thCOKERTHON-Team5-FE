d6d68ae83b155fd8c0a81681b68e869277b3a1b4
chaewon41 <chaew0m3x@gmail.com>
2026-01-23 22:26:19 +0900

    fix: update greeting text

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Layout from './components/Layout';
 function App() {
   return (
     <Layout>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!!</h1>
     </Layout>
   );
 }
