3b25315a7a2d2496ba034b2b45865691dce84aa0
chaewon41 <chaew0m3x@gmail.com>
2026-01-24 01:35:53 +0900

    feat: enhance routing with new pages

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,8 +4,9 @@ function Layout() {
   return (
     <div className="min-h-dvh w-full bg-gray-100 flex justify-center">
       {/* 실제 앱 화면(폭 제한) */}
-      <div id="app-shell" className="w-full max-w-[430px] min-h-dvh bg-white shadow-lg">
-        {/*메인 콘텐츠 영역*/}
+
+      <div id="app-shell" className="w-full max-w-[430px] min-h-dvh bg-white shadow-lg flex flex-col relative">
+        {/* 메인 콘텐츠 영역 */}
         <main className="flex-1 overflow-y-auto">
           <Outlet />
         </main>
