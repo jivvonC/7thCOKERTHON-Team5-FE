ecc0b7ffe338af83c0a32ef961f1a69a7ec43193
jivvonC <jiwonchon07@gmail.com>
2026-01-23 23:51:50 +0900

    feat: browser routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import './App.css';
-import Layout from './components/Layout';
+import Splash from './pages/Splash.tsx';
+import Login from './pages/Login.tsx';
+import StartPage from './pages/StartPage.tsx';
+import JoinPage from './pages/JoinPage.tsx';
+import JoinListPage from './pages/JoinListPage.tsx';
+import Layout from './components/Layout.tsx';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
   return (
-    <Layout>
-      <h1 className="text-3xl font-bold underline">Hello world!!</h1>
-    </Layout>
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<StartPage />}></Route>
+        <Route path="/join" element={<JoinPage />}></Route>
+        <Route path="/joinlist" element={<JoinListPage />}></Route>
+        <Route path="/splash" element={<Splash />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+      </Route>
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
   );
 }
 
